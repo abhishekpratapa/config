0e3453022be3bb7e53a1611804d5bda469005340
Zach Nation <zach@graphlab.com>
2014-05-26 13:08:11 -0700

    Use reverse colors in xterm (black bg)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -25,7 +25,7 @@ import qualified Data.Map        as M
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal = "xterm -fa \"Terminus:size=10:antialias=true\""
+myTerminal = "xterm -rv -fa \"Terminus:size=10:antialias=true\""
 
 
 ------------------------------------------------------------------------
