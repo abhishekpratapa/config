8ac9ea0b1d76a2dd1479241cc936b72f4e2d2051
Zach Nation <znation@apple.com>
2019-05-20 15:32:07 -0700

    Restrict symlinks to top-level config dir

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Add dotfile and other symlinks to home directory
-find `pwd` -name ".*" | grep -v "\/\.git$" | xargs -I {} ln -fs {} ~
+find `pwd` -name ".*" -maxdepth 1 | grep -v "\/\.git$" |  xargs -I {} ln -fs {} ~
 
 # Install necessary plugins/themes
 test -d ~/.tmux/themes/nord-tmux || git clone https://github.com/arcticicestudio/nord-tmux ~/.tmux/themes/nord-tmux
