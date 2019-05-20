f79602f28fc9369ba052c6e3f06e0bac552d4922
Zach Nation <znation@apple.com>
2019-05-20 14:42:58 -0700

    Add Vundle installation to setup script

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ find `pwd` -name ".*" | grep -v "\/\.git$" | xargs -I {} ln -fs {} ~
 
 # Install necessary plugins/themes
 test -d ~/.tmux/themes/nord-tmux || git clone https://github.com/arcticicestudio/nord-tmux ~/.tmux/themes/nord-tmux
+test -d ~/.vim/bundle/Vundle.vim || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
