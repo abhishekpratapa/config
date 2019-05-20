b3d8406808bf5eb184911a3d7a52a84a6a689f91
Zach Nation <znation@apple.com>
2019-05-20 14:41:44 -0700

    Add nord-tmux installation to setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Add dotfile and other symlinks to home directory
 find `pwd` -name ".*" | grep -v "\/\.git$" | xargs -I {} ln -fs {} ~
+
+# Install necessary plugins/themes
+test -d ~/.tmux/themes/nord-tmux || git clone https://github.com/arcticicestudio/nord-tmux ~/.tmux/themes/nord-tmux
